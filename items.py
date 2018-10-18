1d660aec902df82565bff724383faf5a17d16b05
André Flemming <andre.flemming@kisters.de>
2018-10-18 12:39:20 +0200

    Add: CentOS and Debian install

--- items.py
+++ items.py
@@ -4,19 +4,29 @@ from os.path import dirname
 # noinspection PyGlobalUndefined
 global node
 
-pkg_apt = {
+packages = {
     "nginx": {
         "installed": True,
     },
     "openssl": {
         "installed": True,
-    }
+    },
 }
 
-#TODO: systemv compatible
+pkg_apt = {}
+pkg_yum = {}
+
+for pkg, config in packages.items():
+    if node.os == "debian":
+        if pkg not in pkg_apt.keys():
+            pkg_apt[pkg] = config
+    if node.os == "centos":
+        if pkg not in pkg_yum.keys():
+            pkg_yum[pkg] = config
+
+# TODO: systemv compatible
 svc_systemd = {
     "nginx": {
-        'needs': ['pkg_apt:nginx'],
     }
 }
 
@@ -25,12 +35,11 @@ actions = {}
 symlinks = {}
 files = {}
 
-
 if 'nginx' in node.metadata:
     for vhost_name in node.metadata['nginx'].get('sites', {}):
         vhost = node.metadata['nginx']['sites'][vhost_name]
 
-        #Setup SSL
+        # Setup SSL
         if vhost.get('ssl', False):
             directories['/etc/nginx/ssl'] = {
                 'mode': '0755',
@@ -40,26 +49,30 @@ if 'nginx' in node.metadata:
 
             if vhost.get('ssl_snakeoil', False):
                 actions['generate_sneakoil_{}'.format(vhost_name)] = {
-                    'command': 'openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 '\
-                               '-subj "{}" -keyout {} -out {}'.format(\
-                        "/C=DE/ST=NRW/L=Aachen/O=ScoutNet/CN={}".format(vhost_name),\
-                        '/etc/nginx/ssl/{}.key'.format(vhost_name),\
-                        '/etc/nginx/ssl/{}.crt'.format(vhost_name),\
-                    ),
-                    'unless': 'test -f {} && test -f {}'\
-                        .format('/etc/nginx/ssl/{}.crt'.format(vhost_name),'/etc/nginx/ssl/{}.key'.format(vhost_name)),
-                    'needs': ["pkg_apt:openssl"],
+                    'command': 'openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 '
+                               '-subj "{}" -keyout {} -out {}'.format(
+                                    "/C=DE/ST=NRW/L=Aachen/O=Kisters AG/CN={}".format(vhost_name),
+                                    '/etc/nginx/ssl/{}.key'.format(vhost_name),
+                                    '/etc/nginx/ssl/{}.crt'.format(vhost_name), ),
+                    'unless': 'test -f {} && test -f {}'.format('/etc/nginx/ssl/{}.crt'.format(vhost_name),
+                                                                '/etc/nginx/ssl/{}.key'.format(vhost_name)),
                     'cascade_skip': False,
                     'needed_by': [
-                        "pkg_apt:nginx",
                         "svc_systemd:nginx"
                     ],
                 }
+
+                if node.os == "debian":
+                    actions['generate_sneakoil_{}'.format(vhost_name)].get('need', []).append('pkg_apt:openssl')
+                    actions['generate_sneakoil_{}'.format(vhost_name)]['need_by'].append('pkg_apt:nginx')
+                if node.os == "centos":
+                    actions['generate_sneakoil_{}'.format(vhost_name)].get('need', []).append('pkg_yum:openssl')
+                    actions['generate_sneakoil_{}'.format(vhost_name)].get('need_by', []).append('pkg_yum:nginx')
             else:
                 # TODO: Implement LetsEncrypt
                 continue
 
-    #Write vHost file
+    # Write vHost file
     files['/etc/nginx/sites-available/{}.conf'.format(vhost_name)] = {
         'source': 'etc/nginx/sites-available/template.conf',
         'content_type': 'mako',
@@ -72,7 +85,7 @@ if 'nginx' in node.metadata:
         ],
     }
 
-    #Enable vHost
+    # Enable vHost
     if vhost.get('enabled', False):
         symlinks['/etc/nginx/sites-enabled/{}.conf'.format(vhost_name)] = {
             'group': 'root',
@@ -90,7 +103,7 @@ if 'nginx' in node.metadata:
             ],
         }
 
-    #Create vHost dir
+    # Create vHost dir
     directories["/var/www/{}/public_html".format(vhost_name)] = {
         "mode": "755",
     }
